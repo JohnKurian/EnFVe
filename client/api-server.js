f8174fb691ebd0700fc154fc1d71fa4ec01ba027
John Joy <johnjoykurian@gmail.com>
2020-05-28 00:52:35 +0200

    start create report flow

--- client/api-server.js
+++ client/api-server.js
@@ -6,6 +6,7 @@ const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 const authConfig = require("./src/auth_config.json");
 var bodyParser = require('body-parser')
+const axios = require('axios')
 
 const app = express();
 
@@ -44,5 +45,69 @@ app.post("/api/external", checkJwt, (req, res) => {
     msg: "Your access token was successfully validated!"
   });
 });
+
+
+app.post("/api/get_reports", checkJwt, (req, res) => {
+  console.log(req.user)
+  console.log(req.body)
+  axios.post('http://0.0.0.0:16000/get_reports', {
+    hashtags: req.body.hashtags,
+    user: req.body.user
+  })
+      .then((resp) => {
+        console.log(`statusCode: ${resp.statusCode}`)
+        res.send({
+            reports: resp['report_list']
+          });
+
+        // console.log(res)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+
+
+
+  res.send({
+    msg: "Your access token was successfully validated!"
+  });
+});
+
+
+
+app.post("/api/createreport", checkJwt, (req, res) => {
+  console.log(req.body)
+  const axios = require('axios')
+
+  axios.post('http://0.0.0.0:16000/', {
+    hashtags: req.body.hashtags,
+    user: req.body.user
+  })
+      .then((res) => {
+        console.log(`statusCode: ${res.statusCode}`)
+        // console.log(res)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+
+
+  axios.post('http://0.0.0.0:16000/update_user', {
+    user: req.body.user
+  })
+      .then((res) => {
+        console.log(`statusCode: ${res.statusCode}`)
+        // console.log(res)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+
+  res.send({
+    msg: "report has been successfully created!"
+  });
+});
+
+
 
 app.listen(port, () => console.log(`API Server listening on port ${port}`));
--- client/src/views/CreateReport.js
+++ client/src/views/CreateReport.js
@@ -6,34 +6,81 @@ import {NavLink} from "reactstrap";
 
 import Search from "antd/lib/input/Search";
 
-const { TextArea } = Input;
 
-class CreateReport extends React.Component {
-    state = {
-        value: '',
+import { Button } from "reactstrap";
+import Highlight from "../components/Highlight";
+import { useAuth0 } from "../react-auth0-spa";
+import config from "../auth_config.json";
+import TwitterDashboard from "./TwitterDashboard";
+
+
+
+const { apiOrigin = "http://127.0.0.1:3001" } = config;
+
+const CreateReport = () => {
+    const [showResult, setShowResult] = useState(false);
+    const [apiMessage, setApiMessage] = useState("");
+    const { getTokenSilently, user } = useAuth0();
+
+    console.log('user:', user)
+
+    const callApi = async (value) => {
+        try {
+            const token = await getTokenSilently();
+
+            const response = await fetch(`${apiOrigin}/api/createreport`, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'hashtags': [value],
+                    'user': user
+                })
+            });
+
+            const responseData = await response.json();
+
+            setShowResult(true);
+            setApiMessage(responseData);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
-    onChange = ({ target: { value } }) => {
-        this.setState({ value });
-    };
+    return (
+        <>
+            <div className="mb-5">
+                <h1>External API</h1>
+                <p>
+                    Ping an external API by clicking the button below. This will call the
+                    external API using an access token, and the API will validate it using
+                    the API's audience value.
+                </p>
 
-    onSearchClicked(value) {
-        console.log('inside', value)
-    }
+                <Button color="primary" className="mt-5" onClick={callApi}>
+                    Ping API
+                </Button>
 
-    render() {
-        const { value } = this.state;
-
-        return (
-            <div>
                 <Search
                     placeholder="input search text"
-                    onSearch={value => this.onSearchClicked(value)}
+                    onSearch={value => callApi(value)}
                     style={{ width: 200 }}
                 />
+
             </div>
-        )
-    }
-}
+
+            <div className="result-block-container">
+                <div className={`result-block ${showResult && "show"}`}>
+                    <h6 className="muted">Result</h6>
+                    <Highlight>{JSON.stringify(apiMessage, null, 2)}</Highlight>
+                </div>
+            </div>
+
+        </>
+    );
+};
 
 export default CreateReport;

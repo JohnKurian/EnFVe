475708fb83f49e4c60ffb8c626e4b37166202767
John Joy <johnjoykurian@gmail.com>
2020-05-28 02:39:57 +0200

    add fetch reports logic

--- client/src/views/Reports.js
+++ client/src/views/Reports.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { QuestionCircleTwoTone } from '@ant-design/icons';
 import {NavLink as RouterNavLink} from "react-router-dom";
 import {NavLink} from "reactstrap";
+import { useAuth0 } from "../react-auth0-spa";
 
 
 
@@ -11,6 +12,10 @@ import {
     Link,
     Route // for later
 } from 'react-router-dom'
+import config from "../auth_config.json";
+
+
+const { apiOrigin = "http://localhost:3001" } = config;
 
 const axios = require('axios')
 
@@ -24,30 +29,54 @@ function Topic () {
     )
 }
 
-class Reports extends React.Component {
-    state = {
-        value: '',
+const Reports = () => {
+    const { getTokenSilently, user } = useAuth0();
+
+    const [reports, setReports] = useState([]);
+
+
+    const callApi = async () => {
+        try {
+            const token = await getTokenSilently();
+
+            const response = await fetch(`${apiOrigin}/api/get_reports`, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'user_profile': user
+                })
+            });
+
+            const responseData = await response.json();
+            setReports(responseData['reports']);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
-    componentDidMount() {
-        document.title = `You clicked ${this.state.count} times`;
-    }
+    useEffect(() => {
+        callApi()
+    }, []);
 
-    onChange = ({ target: { value } }) => {
+
+    const onChange = ({ target: { value } }) => {
         this.setState({ value });
     };
 
-    render() {
-        const { value } = this.state;
 
         const topics = [{'name': 'report 1', 'id': 1}, {'name': 'report 2', 'id': 2}, {'name': 'report 3', 'id': 3}]
+
 
         return (
             <div>
                 <ul>
-                    {topics.map(({ name, id }) => (
+                    {reports && reports.map(({ hashtags, id }) => (
                         <li key={id}>
-                            <Link to={`/reports/${id}`}>{name}</Link>
+                            <Link to={`/reports/${id}`}>{id}</Link>
                         </li>
                     ))}
                 </ul>
@@ -56,7 +85,7 @@ class Reports extends React.Component {
                 {/*<Route path={`/reports/:reportId`} component={Topic}/>*/}
             </div>
         )
-    }
+
 }
 
 export default Reports;

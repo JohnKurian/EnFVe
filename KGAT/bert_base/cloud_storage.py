69f9393c4b6e670a134ef0e41c73af1e9fd8a0f4
Gaurang Sharma <43086300+GaurangSharma44@users.noreply.github.com>
2020-07-22 17:24:11 +0200

    Update cloud_storage.py

--- KGAT/bert_base/cloud_storage.py
+++ KGAT/bert_base/cloud_storage.py
@@ -1,3 +1,22 @@
+import requests
+
+url = 'https://storage.googleapis.com/kgat_saved_model/vocab.txt'
+r = requests.get(url, allow_redirects=True)
+open('EnFVe/KGAT/bert_base/vocab.txt', 'wb').write(r.content)
+
+url = 'https://storage.googleapis.com/kgat_saved_model/bert_config.json'
+r = requests.get(url, allow_redirects=True)
+open('EnFVe/KGAT/bert_base/bert_config.json', 'wb').write(r.content)
+
+url = 'https://storage.googleapis.com/kgat_saved_model/pytorch_model.bin'
+r = requests.get(url, allow_redirects=True)
+open('EnFVe/KGAT/bert_base/pytorch_model.bin', 'wb').write(r.content)
+
+url = 'https://storage.googleapis.com/kgat_saved_model/model.best.pt'
+r = requests.get(url, allow_redirects=True)
+open('EnFVe/KGAT/model.best.pt', 'wb').write(r.content)
+
+'''
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/content/KernelGAT/kgat/JADGS-687c611bc710.json"
 
@@ -26,3 +45,4 @@ download_blob("kgat_saved_model", "vocab.txt", "EnFVe/KGAT/bert_base/vocab.txt")
 download_blob("kgat_saved_model", "model.best.pt", "EnFVe/KGAT/model.best.pt")
 download_blob("kgat_saved_model", "pytorch_model.bin", "EnFVe/KGAT/bert_base/pytorch_model.bin")
 download_blob("kgat_saved_model", "vocab.txt", "EnFVe/KGAT/bert_base/vocab.txt")
+'''
